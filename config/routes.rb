ae9aabc3305c93f58be556fe297e7df166f65bac
Michael Antis <antis.michael@gmail.com>
2014-11-23 14:08:45 -0500

    changed root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'brooklyn_pizza/index'
+  root 'brooklyn_pizza#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
