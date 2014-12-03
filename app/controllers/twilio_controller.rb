4c480e93f5f606e05f5f4f21b2bdbd5f3c030ec6
Michael Antis <antis.michael@gmail.com>
2014-12-03 11:13:20 -0500

    Added Twilio gem

---
+++ app/controllers/twilio_controller.rb
@@ -0,0 +1,17 @@
+require 'twilio-ruby'
+
+class TwilioController < ApplicationController
+  include Webhookable
+
+  after_filter :set_header
+
+  skip_before_action :verify_authenticity_token
+
+  def voice
+    response = Twilio::TwiML::Response.new do |r|
+      r.Say 'Hey there, Congrats on integrating Twilio'
+    end
+
+  render_twiml response
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
   root 'brooklyn_pizza#index'
+  post 'twilio/voice' => 'twilio#voice'
 
 end
