b0e0b1a94331acf548443c3c921500cbddb128b3
taimurknaziri <taimurknaziri@gmail.com>
2014-12-04 12:54:36 -0500

    Added landing page layout, installed devis, along with additional styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  
-  root 'brooklyn_pizza#index'
-
+	devise_for :users
+	devise_scope :user do
+	  	authenticated :user do
+	    	root :to => 'brooklyn_pizza#index', as: :authenticated_root
+	  	end
+	  	unauthenticated :user do
+	    	root :to => 'devise/registrations#new', as: :unauthenticated_root
+	  	end
+	end
 end
