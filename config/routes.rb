9696a834e5675a9b3f9e63100252e0701008a809
Michael Antis <antis.michael@gmail.com>
2014-12-09 16:36:21 -0500

    Updated home page route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
 	devise_for :users
 	devise_scope :user do
 	  	authenticated :user do
-	    	root :to => 'brooklyn_pizza#index', as: :authenticated_root
+	    	root :to => 'rangers#index', as: :authenticated_root
+	    	# root :to => 'brooklyn_pizza#index', as: :authenticated_root
+
 	  	end
 	  	unauthenticated :user do
 	    	root :to => 'devise/registrations#new', as: :unauthenticated_root
