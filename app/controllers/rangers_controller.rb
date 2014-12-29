b91843c42eaaac876fec30313fb656f12444e868
Michael Antis <antis.michael@gmail.com>
2014-12-29 09:27:19 -0500

    Update README

--- app/controllers/rangers_controller.rb
+++ app/controllers/rangers_controller.rb
@@ -22,44 +22,44 @@ class RangersController < ApplicationController
 
     for i in 1..9
       case i
-      when 1
-        @game_number = @recent_game_tr.at_css("td:nth-child(#{i})").text
-      when 2
-        @date_played = Date.parse(@recent_game_tr.at_css("td:nth-child(#{i})"))
-        @next_faceoff = Date.parse(@next_game_tr.at_css("td:nth-child(#{i})"))
-        @next_faceoff_formatted = @next_faceoff.strftime("%a. %m/%-d/%Y")
-      when 3
-        @faceoff_time = @recent_game_tr.at_css("td:nth-child(#{i})").text
-        @next_game_time = @next_game_tr.at_css("td:nth-child(#{i})").text
-      when 4
-        @location = @recent_game_tr.at_css("td:nth-child(#{i})").text
-        if @location.empty?
-          @location = 'vs.'
-        else
-          @location = '@'
-        end
-        @next_location = @next_game_tr.at_css("td:nth-child(#{i})").text
-        if @next_location.empty?
-          @next_location = 'vs.'
-        else
-          @next_location = '@'
-        end
-      when 5
-        @recent_opponent = @recent_game_tr.at_css("td:nth-child(#{i})").text
-        @next_opponent = @next_game_tr.at_css("td:nth-child(#{i})").text
-      when 6
-        @result = @recent_game_tr.at_css("td:nth-child(#{i})").text
-        if @result == 'W'
-          @result = 'Won'
-        else
-          @result = 'Lost'
-        end
-      when 7
-        @result_variation = @recent_game_tr.at_css("td:nth-child(#{i})").text
-      when 8
-        @rangers = @recent_game_tr.at_css("td:nth-child(#{i})").text
-      when 9
-        @opponent = @recent_game_tr.at_css("td:nth-child(#{i})").text
+        when 1
+          @game_number = @recent_game_tr.at_css("td:nth-child(#{i})").text
+        when 2
+          @date_played = Date.parse(@recent_game_tr.at_css("td:nth-child(#{i})"))
+          @next_faceoff = Date.parse(@next_game_tr.at_css("td:nth-child(#{i})"))
+          @next_faceoff_formatted = @next_faceoff.strftime("%a. %m/%-d/%Y")
+        when 3
+          @faceoff_time = @recent_game_tr.at_css("td:nth-child(#{i})").text
+          @next_game_time = @next_game_tr.at_css("td:nth-child(#{i})").text
+        when 4
+          @location = @recent_game_tr.at_css("td:nth-child(#{i})").text
+          if @location.empty?
+            @location = 'vs.'
+          else
+            @location = '@'
+          end
+          @next_location = @next_game_tr.at_css("td:nth-child(#{i})").text
+          if @next_location.empty?
+            @next_location = 'vs.'
+          else
+            @next_location = '@'
+          end
+        when 5
+          @recent_opponent = @recent_game_tr.at_css("td:nth-child(#{i})").text
+          @next_opponent = @next_game_tr.at_css("td:nth-child(#{i})").text
+        when 6
+          @result = @recent_game_tr.at_css("td:nth-child(#{i})").text
+          if @result == 'W'
+            @result = 'Won'
+          else
+            @result = 'Lost'
+          end
+        when 7
+          @result_variation = @recent_game_tr.at_css("td:nth-child(#{i})").text
+        when 8
+          @rangers = @recent_game_tr.at_css("td:nth-child(#{i})").text
+        when 9
+          @opponent = @recent_game_tr.at_css("td:nth-child(#{i})").text
       end
     end         
   end
