25202740b86edf328ed9e661f1ae8b01fdbccc5e
Michael Antis <antis.michael@gmail.com>
2014-12-31 10:14:21 -0500

    Change route after sign up

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,6 +6,10 @@ class RegistrationsController < Devise::RegistrationsController
     params.require(:user).permit(:fname, :lname, :phone, :email, :password, :password_confirmation)
   end
 
+  def after_sign_up_path(resource)
+    '/rangers'
+  end
+
   def account_update_params
     params.require(:user).permit(:fname, :lname, :phone, :email, :password, :password_confirmation, :encrypted_password)
   end
